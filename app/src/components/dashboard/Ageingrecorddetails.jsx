72638908aa39211a4a3b40f8c63fd153fa0dec40
PremQTIS <128667468+PremBhooma@users.noreply.github.com>
2026-01-21 16:44:44 +0530

    Create Ageingrecorddetails.jsx

---
+++ app/src/components/dashboard/Ageingrecorddetails.jsx
@@ -0,0 +1,11 @@
+import React, { useEffect, useState } from 'react';
+
+const Ageingrecorddetails = () => {
+
+
+  return (
+    <div>Single Ageing Details</div>
+  );
+};
+
+export default Ageingrecorddetails;
