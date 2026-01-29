57af184a404bc2a92396f73aca3888f2d5b86458
PremQTIS <128667468+PremBhooma@users.noreply.github.com>
2026-01-29 18:44:05 +0530

    Update config.jsx

--- app/src/config.jsx
+++ app/src/config.jsx
@@ -2,7 +2,7 @@ const config = {
     // main_url: 'http://localhost:3003',
     // api_url: 'http://localhost:5001'
     main_url: 'https://econestcrm.techdino.in',
-    api_url: 'https://econestcrmapi.techdino.in'
+    api_url: 'https://econest-backend.vercel.app'
 }
 
 export default config;
